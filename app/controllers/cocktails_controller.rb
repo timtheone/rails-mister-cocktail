9fea55c52dcdcfb0299070e7614eea5645d87758
Tim Almamedov <tim.almamedov@gmail.com>
2017-05-25 16:12:35 +0200

    mr cocktail done no frontend

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,25 @@
+class CocktailsController < ApplicationController
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+    @doses = @cocktail.doses
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.create(cocktail_params)
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+end
