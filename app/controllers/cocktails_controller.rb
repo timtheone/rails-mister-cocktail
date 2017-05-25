ad3d991e96634aa9fcd73169f49eeccac2fbca17
Tim Almamedov <tim.almamedov@gmail.com>
2017-05-25 16:20:48 +0200

    validation to creating a cocktail added

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -13,8 +13,12 @@ class CocktailsController < ApplicationController
   end
 
   def create
-    @cocktail = Cocktail.create(cocktail_params)
-    redirect_to cocktail_path(@cocktail)
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
   end
 
   private
